6d266d2a6049079d3ce05c8ef654d3bb1bfe6f88
Ezequiel Werner <eze210@gmail.com>
2023-05-04 11:33:23 -0300

    Add requires com.google.common

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,6 +15,7 @@ module org.mule.runtime.http.policy.api {
   
   requires org.mule.runtime.api;
   requires org.mule.runtime.policy.api;
+  requires com.google.common;
   
   exports org.mule.runtime.http.policy.api;
 
